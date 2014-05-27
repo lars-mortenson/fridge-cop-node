2e6d97a6a56cb75f1f33bb31f7a0873c4e99a1b1
babelphish <babelphish@fridge-cop.com>
2014-05-26 22:44:35 -0400

    fixes to how server handles dev vs prod

--- broadcast.js
+++ broadcast.js
@@ -18,43 +18,28 @@ d.on('error', function(err) {
 var endPoint = "/state_changes"
 
 d.run(function() {
-var prod_client = io
-        .of(endPoint)
-        .on('connection', function(socket) {
-                console.log('client connect')
-        })
+	var prod_client = io
+			.of(endPoint)
+			.on('connection', function(socket) {
+					console.log('client connect')
+			})
 
-var dev_client = io
-        .of('/dev' + endPoint)
-        .on('connection', function(socket) {
-                console.log('dev client connect')
-        })
+	rest.post('/state_change_broadcast', function(req, res, next) {
 
-rest.post('/state_change_broadcast', function(req, res, next) {
-        env = req.query.environment
+			res.header("Access-Control-Allow-Origin", "*")
+			res.header("Access-Control-Allow-Headers", "x-requested-with")
+			message = req.params.message
 
-        res.header("Access-Control-Allow-Origin", "*")
-        res.header("Access-Control-Allow-Headers", "x-requested-with")
-        message = req.params.message
+			console.log("broadcast: " + message)
+			client_to_send = prod_client
 
-        if (env == "DEV")
-        {
-                console.log("dev broadcast: " + message)
-                client_to_send = dev_client
-        }
-        else if (env == "PROD")
-        {
-                console.log("prod broadcast: " + message)
-                client_to_send = prod_client
-        }
-        client_to_send.emit('new_states',  message)
+			client_to_send.emit('new_states',  message)
 
-        res.send({"error": false })
-        next()
-})
+			res.send({"error": false })
+			next()
+	})
 
-rest.listen(8080, function() {
-        console.log('socket listening')
-})
-
+	rest.listen(8081, function() {
+			console.log('socket listening')
+	})
 })
